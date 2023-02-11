9533a1f15b3a5ad10d8e92d9bfe55c49792d6d63
Mike Gray <mike@graywind.org>
2023-02-10 20:19:47 -0600

    fix: upgrade-main workflow

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,6 @@
 const { cdk } = require('projen');
+const { GithubCredentials } = require('projen/lib/github');
+const { NpmAccess } = require('projen/lib/javascript');
 
 const project = new cdk.JsiiProject({
   author: 'Mike Gray',
@@ -15,13 +17,24 @@ const project = new cdk.JsiiProject({
   jestOptions: { updateSnapshot: false },
   githubOptions: { mergify: true },
   workflowNodeVersion: '16',
+  npmAccess: NpmAccess.PUBLIC,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      projenCredentials:
+        GithubCredentials.fromPersonalAccessToken('GITHUB_TOKEN'),
+    },
+  },
 });
-project.compileTask.exec('cp src/pre-commit-config.yaml lib/pre-commit-config.yaml');
+project.compileTask.exec(
+  'cp src/pre-commit-config.yaml lib/pre-commit-config.yaml',
+);
+
+const upgradeMain = project.tryFindObjectFile('.github/workflows/upgrade-main.yml');
+upgradeMain.addOverride('jobs.pr.steps.4.with.token', '${{ secrets.GITHUB_TOKEN }}');
 
 project.addFields({
   main: 'lib/index.js',
   types: 'lib/index.d.ts',
-  public: true,
 });
 
 project.synth();
